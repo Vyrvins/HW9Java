a76b80cd0a4c4908dd40650971e89c3143fd622d
Vyrvins <olya.vyrvinskaya@gmail.com>
2022-11-13 23:32:54 +0300

    new java files

--- src/main/java/ru/netology/post/Main.java
+++ src/main/java/ru/netology/post/Main.java
@@ -2,6 +2,16 @@ package ru.netology.post;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "4444 № 44444444";
+        post.surname = "Иванов";
+        post.patronymic = "Иванович";
+        post.phone = "+7 (999)-999-99-99";
+        post.subscription = true;
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 06;
+        post.birthday.year = 1991;
     }
 }
---
+++ src/main/java/ru/netology/post/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.post;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public Boolean subscription;
+    public FormDate birthday = new FormDate();
+}
